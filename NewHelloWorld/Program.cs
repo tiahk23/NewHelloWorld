741bbc80e4ac396328df31f28b693d5f6494df13
Tiah Keefe <tetered12@gmail.com>
2021-07-12 20:10:55 -0500

    Add project files.

---
+++ NewHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
