e3b35a9c2ec3766a028c6e825bcc0f0af19d77ec
Tiah Keefe <tetered12@gmail.com>
2021-07-12 20:14:14 -0500

    Wrote a new program to collect input of someones name and output the results.

--- NewHelloWorld/Program.cs
+++ NewHelloWorld/Program.cs
@@ -6,7 +6,9 @@ namespace NewHelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
         }
     }
 }
